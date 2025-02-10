b0e1c61a31814b1ea0d10bb6d20e699527823e7f
hglwsp <1289787496@qqcom>
2025-02-10 22:56:00 +0800

    20250211leet1

---
+++ spwang_newleet_withgit/leet_20.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+        stack = []
+        for i in s:
+            if i in dic:
+                stack.append(i)
+            else:
+                if len(stack) == 0 or dic[stack[-1]]!=i:
+                    return False
+                else:
+                    stack.pop()
+        return len(stack) == 0
+
+
+test = Solution()
+print(test.isValid("))"))
